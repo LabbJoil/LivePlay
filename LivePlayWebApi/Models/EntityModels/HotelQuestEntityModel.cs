7dce13a513c9ac2133fcba45cf141b129e201bf6
LabbJoil <lev.t119@gmail.com>
2024-04-21 14:28:27 +0300

    Add HotelQuestEntityModel

--- LivePlayWebApi/Models/EntityModels/HotelQuestEntityModel.cs
+++ LivePlayWebApi/Models/EntityModels/HotelQuestEntityModel.cs
@@ -1,5 +1,16 @@
-﻿namespace LivePlayWebApi.Models.EntityModels;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace LivePlayWebApi.Models.EntityModels;
+
+[Table("HotelQuest")]
 public class HotelQuestEntityModel
 {
+    [Key, Required]
+    public int Id { get; set; }
+    [Required]
+    public int QuestId { get; set; }
+    [Required]
+    public int HotelId { get; set; }
+
 }
