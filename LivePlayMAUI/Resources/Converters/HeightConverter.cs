87493b1b7496c9b934ca0e983d1506395dd0b949
LabbJoil <lev.t119@gmail.com>
2024-05-20 11:43:15 +0300

    Edit Password PE

---
+++ LivePlayMAUI/Resources/Converters/HeightConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LivePlayMAUI.Models.Domain;
+
+namespace LivePlayMAUI.Resources.Converters;
+
+public class HeightConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double doubleValue)
+            return doubleValue / 1.8;
+        return null;
+    }
+
+    public object ConvertBack(object? value, Type? targetType = null, object? parameter = null, CultureInfo? culture = null)
+    {
+        throw new NotImplementedException();
+    }
+}
