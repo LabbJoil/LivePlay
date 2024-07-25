12ca533fb11041bd09755c7839c380caff6ede3f
LabbJoil <lev.t119@gmail.com>
2024-07-26 00:10:45 +0300

    The root name of the server and the front of the projects have been changed. Change docker project

---
+++ LivePlay.Server/LivePlay.Server.WebApi/ProgramExtentions/MigrationExtension.cs
@@ -0,0 +1,15 @@
+﻿
+using LivePlay.Server.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace LivePlay.Server.WebApi.ProgramExtentions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        using LivePlayDbContext dbContext = scope.ServiceProvider.GetRequiredService<LivePlayDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
